fd973fcb0f0fd4b1dfebfa23a7728d64c8f04bb2
mateenshamsi <matinshamsi16@gmail.com>
2025-12-12 18:34:30 +0530

    initialized

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex items-center justify-center '>
+      <Button >Hello</Button>
+    </div>
+  )
+}
+
+export default page
